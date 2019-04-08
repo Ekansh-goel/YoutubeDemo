965c5ad7cb7d7dd2eee70e8005651642881325f7
ekansh goel <ekanshgoel@123.com>
2019-04-09 01:40:51 +0530

    first upload

---
+++ app/src/main/java/com/example/youtubedemo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.youtubedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+public class MainActivity extends YouTubeBaseActivity {
+    YouTubePlayerView youTubePlayerView;
+    Button button;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.btn_play);
+        youTubePlayerView = findViewById(R.id.youtubeid);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("3LiubyYpEUk");
+            }
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+            }
+        };
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youTubePlayerView.initialize(PlayerConfig.API_KEY, onInitializedListener);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/youtubedemo/PlayerConfig.java
@@ -0,0 +1,9 @@
+package com.example.youtubedemo;
+
+public class PlayerConfig {
+    public PlayerConfig() {
+
+    }
+    public static final String API_KEY="ENTER YOUR YOUTUBE API";
+
+}
